a77e91c5de4fed65a192ab3aa814b432a869367f
Cyber Team 3DMente <render3dmente@gmail.com>
2021-03-28 12:23:38 -0300

    Se implementan test unitarios para los casos de creación de post utilizando mocks

--- endpoints/posts/index.spec.js
+++ endpoints/posts/index.spec.js
@@ -1,7 +1,76 @@
+// {
+//     "userId": 1,
+//     "id": 1,
+//     "title": "Titulo",
+//     "body": "Cuerpo"
+// }
+
+const postHandlers = require('./index') // Se obtienen los Handlres del Post
+
 describe('Endpoints',()=>{
     describe('post',()=>{
-        it.skip('should create',()=>{
+        it('should create',async ()=>{
+            const mockUsers=[
+                {id:1},
+                {id:2}
+            ]
+            const post = {
+                userId: 1,
+                title: "Titulo",
+                body: "Cuerpo"
+            }
+            const req={
+                body:post,
+            }
+            const res = {
+                status: jest.fn().mockReturnThis(),
+                send: jest.fn()
+            }
+            const axios = {
+                get: jest.fn().mockResolvedValue({data:mockUsers}),
+                post: jest.fn().mockResolvedValue({data:{id:1000}}),
+            }
+            await postHandlers({axios}).post(req,res)
 
+            expect(res.status.mock.calls).toEqual([[
+                201
+            ]])
+            expect(res.send.mock.calls).toEqual([[
+                {id:1000}
+            ]])
+            expect(axios.get.mock.calls).toEqual([[
+                'https://jsonplaceholder.typicode.com/users'
+            ]])
+            expect(axios.post.mock.calls).toEqual([[
+                'https://jsonplaceholder.typicode.com/posts',post
+            ]])
+        }),
+        it('should not create',async ()=>{
+            const mockUsers=[
+                {id:1},
+                {id:2}
+            ]
+            const post = {
+                userId: 3,
+                title: "Titulo",
+                body: "Cuerpo"
+            }
+            const req={
+                body:post,
+            }
+            const res = {
+                status: jest.fn().mockReturnThis(),
+                send: jest.fn(),
+                sendStatus: jest.fn()
+            }
+            const axios = {
+                get: jest.fn().mockResolvedValue({data:mockUsers}),
+                post: jest.fn().mockResolvedValue({data:{id:1000}}),
+            }
+            await postHandlers({axios}).post(req,res)
+            expect(axios.post.mock.calls).toEqual([])
+            expect(res.sendStatus.mock.calls).toEqual([[400]])
+            
         })
     })
 })
--- middlewares/authenticate.spec.js
+++ middlewares/authenticate.spec.js
@@ -3,7 +3,7 @@ describe('Middlewares',()=>{
     describe('authenticate',()=>{
         it('Should Have Id One',()=>{
             const req = {
-                header: jest.fn().mockReturnValue(1)
+                header: jest.fn().mockReturnValue("1")
             }
             const res = {
                 sendStatus: jest.fn()
@@ -19,16 +19,14 @@ describe('Middlewares',()=>{
     })
     it('Should fail if user is not the one with id 1',()=>{
         const req = {
-            header: jest.fn().mockReturnValue(2)
+            header: jest.fn().mockReturnValue("2")
         }
         const res = {
             sendStatus: jest.fn()
         }
         const next = jest.fn()
         authenticate(req,res,next)
-        expect(req.header.mock.calls).toEqual([
-            ['user_id']
-        ])
+        expect(req.header.mock.calls).toEqual([['user_id']])
         expect(res.sendStatus.mock.calls).toEqual([[403]])
         expect(next.mock.calls).toEqual([])
     })
